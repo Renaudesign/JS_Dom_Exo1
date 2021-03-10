e229d10bae558b4b2e947bc50c1e47ae09c02710
renaudesign <renaud.sourmail@gmail.com>
2021-03-10 13:54:46 +0100

    opti

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function rollOver(element){
-    let imgTag = element.getAttribute("id")
-    document.getElementById(imgTag).style.border = "3px solid red"
+     element.style.border = "3px solid red"
 }
 function rollOut(element){
-    let imgTag = element.getAttribute("id")
-    document.getElementById(imgTag).style.border = "0px"
+    element.style.border = "0px"
 }
